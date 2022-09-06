9f8d063fc14a0e5b235952e22f742c7e44e5e545
Irina Spasova <irina.spasova.81@gmail.com>
2022-09-06 18:44:46 +0300

    Children

--- John Smilga-React/Section-2-Install-create-react-app/tutorial/src/index.js
+++ John Smilga-React/Section-2-Install-create-react-app/tutorial/src/index.js
@@ -19,32 +19,78 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book
-       img={firstBook.img} 
-       title={firstBook.title} 
-       author={firstBook.author} />
-      <Book 
-      img={secondBook.img} 
-      title={secondBook.title} 
-      author={secondBook.author} />
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}>
+        {/* children  ( children is the content between opening and closing tag)*/}
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, atque.</p>
+      </Book>
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author} />
     </section>
   )
 }
 
 const Book = (props) => {
-  // console.log(props); we can check values in Book in BookLIst
+const {img,title, author, children} = props;
+console.log(props);
   return <article className='book'>
     <img
-      src={props.img}
+      src={img}
       alt=""
     />
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
-    {/* <p>{props.job}</p> */}
-    {/* <p>{props.title}</p> */}
-    {/* <p>{props.number}</p> */}
-    {/* {console.log(props)} */}
-    {/* <p>{let x=6}</p> */}
-    {/* things in {} always have to return value/ should be expressions. Statements, like <p>{let x=6}</p> will throw error, but <p>{6+6}</p> returns value */}
+    {/* {props.children} */}
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+    {children}
   </article>
 }
+
+// Second variant
+// const Book = (props) => {
+//   const {img,title, author} = props;
+//   console.log(props);
+//     return <article className='book'>
+//       <img
+//         src={img}
+//         alt=""
+//       />
+//       {/* {props.children} */}
+//       <h1>{title}</h1>
+//       <h4>{author}</h4>
+//       {props.children}
+//     </article>
+//   }
+
+// First variant:
+// const Book = ({ img, title, author,children }) => {
+
+//   return <article className='book'>
+//     <img
+//       src={img}
+//       alt=""
+//     />
+//     {children}
+//     <h1>{title}</h1>
+//     <h4>{author}</h4>
+//     {children}
+//   </article>
+// }
+
+
+// First variant:
+// const Book = (props) => {
+//   const{img, title,author}=props;
+//     return <article className='book'>
+//       <img
+//         src={img}
+//         alt=""
+//       />
+//       <h1>{title}</h1>
+//       <h4>{author}</h4>
+//    </article>
+//   }
 ReactDOM.render(<BookList></BookList>, document.getElementById('root'));
