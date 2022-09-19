c2eb5d30bb29cd69133c342ccdd3f9d2f9c0d184
Irina Spasova <irina.spasova.81@gmail.com>
2022-09-19 21:42:35 +0300

    decrease ready

---
+++ John Smilga-React/Section-5-Basic-Projects/react-projects-master/14-cart/setup/src/reducer.js
@@ -0,0 +1,54 @@
+const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CART') {
+        return { ...state, cart: [] }
+    }
+    if (action.type === 'REMOVE') {
+        return { ...state, 
+            cart: state.cart.filter((cartItem) => 
+            cartItem.id !== action.payload) }
+    }
+    if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((cartItem) => {
+          if (cartItem.id === action.payload) {
+            return { ...cartItem, amount: cartItem.amount + 1 }
+          }
+          return cartItem
+        })
+        return { ...state, cart: tempCart }
+      }
+      if (action.type === 'DECREASE') {
+        let tempCart = state.cart
+          .map((cartItem) => {
+            if (cartItem.id === action.payload) {
+              return { ...cartItem, amount: cartItem.amount - 1 }
+            }
+            return cartItem
+          })
+          .filter((cartItem) => cartItem.amount !== 0) // removes from cart when count reach 0 
+        return { ...state, cart: tempCart }
+      }
+
+ 
+    return state;
+}
+
+export default reducer;
+
+// this not working:
+
+   // if (action.type === 'INCREASE'){
+    //     let tempCard = state.cart.map((cartItem)=>{
+    //         if(cartItem.id === action.payload){
+    //             return{...cartItem, amount:cartItem.amount+1}
+    //         }
+    //     });
+    //     return {...state, cart:tempCard}
+    // }
+    // if (action.type === 'DECREASE'){
+    //     let tempCard = state.cart.map((cartItem)=>{
+    //         if(cartItem.id === action.payload){
+    //             return{...cartItem, amount:cartItem.amount-1}
+    //         }
+    //     });
+    //     return {...state, cart:tempCard}
+    // }
