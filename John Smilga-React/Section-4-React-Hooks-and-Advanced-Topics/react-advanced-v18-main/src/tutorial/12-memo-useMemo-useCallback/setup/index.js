ae232e0349fd41c803d8f7bb8e4b994ebd17e5f5
Irina Spasova <irina.spasova.81@gmail.com>
2022-09-21 14:46:43 +0300

    Random ready

--- John Smilga-React/Section-4-React-Hooks-and-Advanced-Topics/react-advanced-v18-main/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ John Smilga-React/Section-4-React-Hooks-and-Advanced-Topics/react-advanced-v18-main/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -7,9 +7,28 @@ const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
 
+const calculateMostExpensive = (data) => {
+  return (data.reduce((total, item) => {
+    const price = item.fields.price;
+    if (price >= total) {
+      total = price;
+    }
+    return total
+  }, 0) / 100
+  );
+};
+
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [cart, setCart] = useState(0);
+
+  const addToCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
+const mostExpensive = useMemo(()=>calculateMostExpensive(products),
+[products,])
 
   return (
     <>
@@ -17,22 +36,34 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: '3rem' }}> cart: {cart}</h1>
+      {/*05:30: https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/22435912#questions/14384820 */}
+      {/* <h1>Most expensive : ${calculateMostExpensive(products)}</h1> */}
+      <h1>Most expensive : ${mostExpensive}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
-const BigList = ({ products }) => {
+// memo memorize the initial state and if there is no change in props ( products in this case) - memo prevent re-rendering
+// 07:30 https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/22435894#questions/14384820
+const BigList = React.memo(({ products, addToCart }) => {
+  useEffect(() => {
+    console.log('big list called');
+  });
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
-}
+})
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
+  useEffect(() => {
+    console.log('single product called');
+  });
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
@@ -42,6 +73,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
--- John Smilga-React/Section-4-React-Hooks-and-Advanced-Topics/react-advanced-v18-main/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ John Smilga-React/Section-4-React-Hooks-and-Advanced-Topics/react-advanced-v18-main/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,19 +1,22 @@
+import { useCallback } from 'react';
 import { useState, useEffect } from 'react';
 
 export const useFetch = (url) => {
     const [loading, setLoading] = useState(true)
     const [products, setProducts] = useState([])
   
-    const getProducts = async () => {
+    const getProducts = useCallback(async () => {
       const response = await fetch(url)
       const products = await response.json()
       setProducts(products)
       setLoading(false)
-    }
+    },[url]);
   
     useEffect(() => {
       getProducts()
-    }, [url])
+    }, [url,getProducts])
     
     return{ loading, products };
 };
+// important- we can cause loop here without useCallback!
+// https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/22435914#questions/16545804
