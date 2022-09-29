2e970097f8179241ff15ca0a8485e2b76b3bcb6c
Irina Spasova <irina.spasova.81@gmail.com>
2022-09-29 21:16:04 +0300

    pagination ready

--- John Smilga-React/Section-5-Basic-Projects/react-projects-master/18-pagination/setup/src/utils.js
+++ John Smilga-React/Section-5-Basic-Projects/react-projects-master/18-pagination/setup/src/utils.js
@@ -1,3 +1,15 @@
-const paginate = (followers) => {}
+const paginate = (followers) => {
+    const itemsPerPage = 12;
+    const pages = Math.ceil(followers.length / itemsPerPage);
+    //console.log(pages);
+
+    const newFollowers = Array.from({ length: pages }, (_, index) => {
+        const start = index * itemsPerPage;
+        //console.log(start);
+        return followers.slice(start, start + itemsPerPage);
+    });
+    //console.log(newFollowers);
+    return newFollowers;
+}
 
 export default paginate
