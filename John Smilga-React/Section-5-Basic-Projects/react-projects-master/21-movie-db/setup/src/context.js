6f047d527e5ec5b2c6a86cc26cb4d7b377f739b7
Irina Spasova <irina.spasova.81@gmail.com>
2022-10-03 23:29:16 +0300

    Movie start

--- John Smilga-React/Section-5-Basic-Projects/react-projects-master/21-movie-db/setup/src/context.js
+++ John Smilga-React/Section-5-Basic-Projects/react-projects-master/21-movie-db/setup/src/context.js
@@ -3,6 +3,8 @@ import React, { useState, useContext, useEffect } from 'react'
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 const AppContext = React.createContext()
 
+console.log(API_ENDPOINT);
+
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
