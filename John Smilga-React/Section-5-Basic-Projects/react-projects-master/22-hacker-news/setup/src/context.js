e6d3c2ec622d2e40eef59ff17ec982f16a9c9cdd
Irina Spasova <irina.spasova.81@gmail.com>
2022-10-05 00:24:07 +0300

    Remove ready

--- John Smilga-React/Section-5-Basic-Projects/react-projects-master/22-hacker-news/setup/src/context.js
+++ John Smilga-React/Section-5-Basic-Projects/react-projects-master/22-hacker-news/setup/src/context.js
@@ -9,26 +9,57 @@ import {
 } from './actions'
 import reducer from './reducer'
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+// original:
+ const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+//const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search_by_date?' //- not working, to fix
 
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0,
 }
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING })
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
+  const removeStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id })
+  }
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query })
+  }
+  const handlePage = (value) => {
+    dispatch({ type: HANDLE_PAGE, payload: value })
+  }
   useEffect(() => {
-    fetchStories();
-  }, []);
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
+  }, [state.query, state.page])
 
-  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  return (
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
