1902de9806e958da14c21ffd75ebc09f5336596a
Irina Spasova <irina.spasova.81@gmail.com>
2022-10-15 20:42:20 +0300

    useEffect working

--- 03-Using-React-17-Hooks/start/src/ImageToggleOnScroll.js
+++ 03-Using-React-17-Hooks/start/src/ImageToggleOnScroll.js
@@ -3,6 +3,8 @@ import React, { useEffect, useRef, useState } from "react";
 const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const imageRef = useRef(null);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const isInView = () => {
     const rect = imageRef.current.getBoundingClientRect();
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
@@ -11,6 +13,8 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
   const [inView, setInView] = useState(false);
 
   useEffect(() => {
+    setIsLoading(false);
+    setInView(isInView());
     window.addEventListener("scroll", scrollHandler);
     return () => {
       window.removeEventListener("scroll", scrollHandler);
@@ -21,7 +25,22 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
     setInView(isInView());
   };
 
-  return <img src={inView ? secondaryImg : primaryImg} alt="" ref={imageRef} />;
+  return (
+    <img
+      src={
+        isLoading
+          ? // it is important to have dummy image 02:59 : https://app.pluralsight.com/course-player?clipId=5e6f28fd-e83c-4361-9a16-e7e0b9d897e9
+            "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" // 1x1gif
+          : inView
+          ? secondaryImg
+          : primaryImg
+      }
+      alt=""
+      ref={imageRef}
+      width="200"
+      height="200"
+    />
+  );
 };
 
 export default ImageToggleOnScroll;
