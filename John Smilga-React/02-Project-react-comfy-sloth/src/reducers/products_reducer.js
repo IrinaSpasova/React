7f6b05ef65a5402b8e91c820b6036198e6e2c410
Irina Spasova <irina.spasova.81@gmail.com>
2022-11-01 20:25:13 +0200

    fetch products ready

--- John Smilga-React/02-Project-react-comfy-sloth/src/reducers/products_reducer.js
+++ John Smilga-React/02-Project-react-comfy-sloth/src/reducers/products_reducer.js
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils";
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -18,8 +19,25 @@ const products_reducer = (state, action) => {
     //console.log(action);
     return { ...state, isSidebarOpen: false };
   }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
 
-  return state;
+  // return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
